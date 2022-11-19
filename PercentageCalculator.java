a6a60c0e9e6750d83f1299f07481da5703d7843b
nishantkishan <98928900+nishantkishan@users.noreply.github.com>
2022-11-19 16:37:17 +0530

    Add files via upload

---
+++ PercentageCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PercentageCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float percentage;
+		float total_marks;
+		float scored;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your marks::");
+		scored=sc.nextFloat();
+		
+		System.out.println("Enter total marks::");
+		total_marks=sc.nextFloat();
+		
+		percentage=(float)((scored/total_marks)*100);
+		System.out.println("Percentage::"+percentage);
+		
+
+
+	}
+
+}
